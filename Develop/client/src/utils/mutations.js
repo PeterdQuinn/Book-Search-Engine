13df4dcc81a1f46abb7fffd79138866b378acca1
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-07 02:49:42 -0700

    remove, save, add,remove done with mutations.js

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -32,3 +32,49 @@ mutation saveBook($input: savedBook!) {
         }
     }
 `;    
+
+
+export const ADD_USER = gql`
+
+mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+      token
+    }
+  }
+  `;
+
+  export const REMOVE_BOOK = gql`
+
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId:$bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            # _id
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}
+`;
