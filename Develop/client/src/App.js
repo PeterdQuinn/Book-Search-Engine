f12a23e028462d23f05bf20f478aa8874f1c124c
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-07 21:43:59 -0700

    adding to search books

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -4,6 +4,32 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+///---- add ins for project-----////
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+////---////////////////////////////////////
+
+const client = new ApolloClient({
+  // uri: 'http://localhost:3001/graphql'
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
+
+
+
+
+
+
+
+
 function App() {
   return (
     <Router>
@@ -18,5 +44,6 @@ function App() {
     </Router>
   );
 }
+import { Form } from 'react-bootstrap/lib/Navbar';
 
 export default App;
